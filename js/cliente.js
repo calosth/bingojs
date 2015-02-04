0c93d67863851eb7da0a54a26ef531d50623cf41
walis85300 <walojose46@gmail.com>
2015-02-04 18:58:40 -0430

    se agrego bingo a los cartones

--- js/cliente.js
+++ js/cliente.js
@@ -1,10 +1,10 @@
 var dgram = require('dgram');
 var client = dgram.createSocket('udp4');
 
-client.on('listening', function(){
-	var address = client.address();
-	console.log('UDP client Listening on ' + address.address+ ':' +address.port);
-});
+// client.on('listening', function(){
+// 	var address = client.address();
+// 	console.log('UDP client Listening on ' + address.address+ ':' +address.port);
+// });
 
 client.on('message',function(message,remote){
 	console.log(remote.address + ':' + remote.port + ' - ' + message);
