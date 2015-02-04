e783206ae0f6f0681e238db2910d129e4cfa36f7
walis85300 <walojose46@gmail.com>
2015-02-04 14:26:12 -0430

    Se valido que se ingresaran todos los campos

--- js/crear.js
+++ js/crear.js
@@ -4,9 +4,18 @@ var ifaces = os.networkInterfaces();
 
 var port = 41234;
 
+$("#nombrePartida").on('click',function(){
+	$('#nombrePartida').parent().removeClass("has-error");
+});
+$("#maximoPersonas").on('click',function(){
+	$('#maximoPersonas').parent().removeClass("has-error");
+});
+$("#maximoCartones").on('click',function(){
+	$('#maximoCartones').parent().removeClass("has-error");
+});
+
 $("#submit").on('click',function(){
 	
-
 	var nombrePartida = $("#nombrePartida").val();
 	var maximoDePersonas = $("#maximoPersonas").val();
 	var maximoDeCartones = $("#maximoCartones").val();
@@ -16,8 +25,22 @@ $("#submit").on('click',function(){
 		'contenido': {
 			'ip': ifaces.en1[1].address,
 			'sala': nombrePartida,
+			'maxPersonas': maximoDePersonas,
+			'maxCartones': maximoDeCartones
 		},
 	};
 
-	network.serverUDP(json, port);
+	if(nombrePartida==''){
+		$("#nombrePartida").parent().addClass("has-error");
+	}
+	else if(maximoDePersonas==''){		
+		$("#maximoPersonas").parent().addClass("has-error");
+	}
+	else if(maximoDeCartones==''){		
+		$("#maximoCartones").parent().addClass("has-error");
+	}
+	else{
+		network.serverUDP(json, port);		
+	}	
+
 });
