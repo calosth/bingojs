22b2924189be6a8e6fc54a4950bc4ee0c1331a1a
Eduardo Álvarez <walojose46@gmail.com>
2015-04-02 23:54:49 -0430

    ya funciona en windows

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 global.myIP = function(){
 	var os = require('os');
+	var ifaces = os.networkInterfaces();
 
 	try{
 		if(os.platform() === 'darwin'){
-			var ifaces = os.networkInterfaces();
 			return ifaces.en1[1].address;
+		}
+		else{
+			var alias = 0;
+			var ip;
+			Object.keys(ifaces).forEach(function (ifname) {
+
+			ifaces[ifname].forEach(function (iface) {
+			if ('IPv4' !== iface.family || iface.internal !== false) {
+				// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+				return;
+			}
+			if(alias==0){
+				ip= iface.address;
+			}
+			 alias++;
+			});
+			});
+			return ip;
 		}
 	}catch(err){
 		console.log(err);
@@ -14,4 +32,5 @@ global.myIP = function(){
 
 // global.ip es el IP que se usará cuando sea un servidor
 // globa.myIP es el IP que se usará cuando sea un cliente
+console.log(global.myIP);
 global.ip = global.myIP;
