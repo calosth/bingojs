ec545561fc601e9ecc016bb8d63cf96a0740e824
walis85300 <walojose46@gmail.com>
2015-04-09 18:35:51 -0430

    no se que hicimos

--- js/setting.js
+++ js/setting.js
@@ -1,14 +1,7 @@
-global.portUPD 			= 10022;
-global.portTCP 			= 10022;
-global.ipBroadcast 		= 255.255.255.255;
-global.ipMulticast 		= 239.1.2.3;
-global.portMulticast 	= 5554;
-
 $('#submit').on('click',function(){
-	global.portUPD 			= $("#puertoUDP").text();
-	global.portTCP 			= $("#puertoTCP").text();
-	global.ipBroadcast 		= $('#ipBroadcast').text();
-	global.ipMulticast 		= $('#ipMulticast').text();
-	global.portMulticast 	= $('#portMulticast').text();
+	global.portUPD		= Number($("#puertoUDP").val());
+	global.portTCP		= Number($("#puertoTCP").val());
+	global.ipBroadcast	= $('#ipBroadcast').val();
+	global.ipMulticast		= $('#ipMulticast').val();
+	global.portMulticast	= Number($('#portMulticast').val());
 });
-
